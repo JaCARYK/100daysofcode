802506bb9bfa627a22d5c8a0b72ecebc45e5c8ab
JaCARYK <66707101+JaCARYK@users.noreply.github.com>
2025-08-23 00:39:32 -0700

    first project of the course

---
+++ projectone.py
@@ -0,0 +1,3 @@
+input("type enter to run the final project you will build ")
+print("Welcome to the Band Name Generator")
+print("Your band name could be " + input("whats the name of the city you grew up in? \n") + input("whats your pets name "))
